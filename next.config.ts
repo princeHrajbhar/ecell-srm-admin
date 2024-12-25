c234b7e5c001bdb4aeea0d07ae92c30ba370e6bb
princeHrajbhar <princerajbharhariram@gmail.com>
2024-12-25 23:12:28 +0530

    next-config

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,32 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,  // Enables React strict mode
-//   swcMinify: true,       // Enables SWC-based minification
-//   images: {
-//     domains: ["res.cloudinary.com"], 
-//     // Allow external image domains
-//   },
-//   env: {
-//     API_BASE_URL: process.env.API_BASE_URL, // Add environment variables
-//   },
-// };
-
-// module.exports = nextConfig;
-
-/*
-@type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true,  // Enables React strict mode
+  swcMinify: true,       // Enables SWC-based minification
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**', // Match all paths under res.cloudinary.com
-      },
-    ],
+    domains: ["res.cloudinary.com"], 
+    // Allow external image domains
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL, // Add environment variables
   },
 };
 
 module.exports = nextConfig;
+
+/*
+@type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         port: '',
+//         pathname: '/**', // Match all paths under res.cloudinary.com
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
